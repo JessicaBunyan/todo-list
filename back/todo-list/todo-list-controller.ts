4de797c06907af9859a53f5d72e3c346fe39a136
Jessica Bunyan <jessicabunyan93@gmail.com>
2020-08-08 12:30:49 +0100

    initial API implementation

--- back/todo-list/todo-list-controller.ts
+++ back/todo-list/todo-list-controller.ts
@@ -1,12 +1,50 @@
-import * as express from "express";
 import { TodoList } from "./todo-list.model";
+import { Router, Request, Response } from "express";
 
 const lists: { [key: string]: TodoList } = {};
 
-export const routes = function (router: express.Router) {
-  router.post("/tournament/");
-  // router.post('/tournament/new', makeValidateBody(ITournamentDB), newTournament)
-  // router.get('/tournament/all', getAllTournaments)
-  // router.get('/tournament/:id', getTournament)
-  // router.get('/tournament/upcoming', upcomingTournaments)
+export const routes = function (router: Router) {
+  router.post("/list", saveList);
+  router.get("/list/:id", getList);
+  router.patch("/list/:listId/:itemId/:state", updateItem);
 };
+
+async function saveList(req: Request, res: Response) {
+  const list: TodoList = req.body;
+
+  list.id = list.id || getId();
+  lists[list.id] = req.body;
+
+  return res.send(list);
+}
+
+async function getList(req: Request, res: Response) {
+  const id = req.params.id;
+
+  const list = lists[id];
+  if (!list) {
+    return res.status(404).send("List not found");
+  }
+}
+
+async function updateItem(req: Request, res: Response) {
+  const { listId, itemId, state } = req.params;
+
+  const list = lists[listId];
+  if (!list) {
+    return res.status(400).send("Invalid update - list does not exist");
+  }
+  const item = list.items.find((i) => i.id == itemId);
+
+  if (!item) {
+    return res.status(400).send("Invalid update - list item does not exist");
+  }
+
+  item.done = Boolean(state);
+
+  res.send(item);
+}
+
+function getId() {
+  return Math.random().toString(36).substring(2, 9);
+}
--- back/todo-list/todo-list.model.ts
+++ back/todo-list/todo-list.model.ts
@@ -1,10 +1,12 @@
 export interface TodoList {
+  id: string;
   name: string;
   createdOn: number;
   items: TodoListItem[];
 }
 
 export interface TodoListItem {
+  id: string;
   text: string;
   done: boolean;
 }
